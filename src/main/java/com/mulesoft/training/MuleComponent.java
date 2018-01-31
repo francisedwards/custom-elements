d07db8290bce1c3c7ebf8e74684575df96ac8b00
Edwards, Francis <Francis.Edwards@sgbd.co.uk>
2018-01-31 10:02:36 +0000

    Java Code handles payload

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -1,22 +1,42 @@
 package com.mulesoft.training;
 
+import org.mule.api.annotations.param.InboundHeaders;
+import org.mule.api.annotations.param.Payload;
+
+import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class MuleComponent {
 
-  public Map<String,String> processMap() {
-    // processMap implementation
-    return null;
-  }
+	public Map<String, String> processMap(Map<String, String> input) {
+		// processMap implementation
+		input.put("processed by", "processMap");
+		return input;
+	}
 
-  public Map<String,String> processArray() {
-    // processArray implementation
-    return null;
-  }
+	public Map<String, String> processArray(List<String> input) {
+		// processArray implementation
+		Map<String, String> output = new HashMap<String, String>();
+		output.put("message", input.get(0));
+		output.put("processed by", "processArray");
+		return output;
+	}
 
-  public Map<String,String> processString() {
-    // processString implementation
-    return null;
-  }
+	public Map<String, String> processString(String input) {
+		// processString implementation
+		Map<String, String> output = new HashMap<String, String>();
+		output.put("message", input);
+		output.put("processed by", "processString");
+		return output;
+	}
 
-} 
+	public Map<String, String> processAll(@Payload Object input, @InboundHeaders("http.method") String method) {
+		// processString implementation
+		Map<String, String> output = new HashMap<String, String>();
+		output.put("message", input.toString());
+		output.put("processed by", "processAll");
+		output.put("method", method);
+		return output;
+	}
+}
